db97c985f42e97096c03305753e3e72ea187d5d4
Navid Farhadi <navid@navidfarhadi.com>
2018-09-05 12:21:12 +0200

    HMM0 works for all test cases on kattis

--- Assignments/A1/HMM0/HMM0.java
+++ Assignments/A1/HMM0/HMM0.java
@@ -1,6 +1,51 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class HMM0
 {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Locale.setDefault(new Locale("en", "US"));
+
+		Scanner sc = new Scanner(System.in);
+
+		int transitionMatNumRows = sc.nextInt();
+		int transitionMatNumCols = sc.nextInt();
+		double[][] transitionMat = new double[transitionMatNumRows][transitionMatNumCols]; 
+
+		for (int i = 0; i < transitionMatNumRows; i++) {
+			for (int j = 0; j < transitionMatNumCols; j++) {
+				transitionMat[i][j] = sc.nextDouble();
+			}
+		}
+
+		int emissionMatNumRows = sc.nextInt();
+		int emissionMatNumCols = sc.nextInt();
+		double[][] emissionMat = new double[emissionMatNumRows][emissionMatNumCols]; 
+
+		for (int i = 0; i < emissionMatNumRows; i++) {
+			for (int j = 0; j < emissionMatNumCols; j++) {
+				emissionMat[i][j] = sc.nextDouble();
+			}
+		}
+
+		int initStateMatNumRows = sc.nextInt();
+		int initStateMatNumCols = sc.nextInt();
+		double[][] initStateMat = new double[initStateMatNumRows][initStateMatNumCols];
+
+		for (int i = 0; i < initStateMatNumCols; i++) {
+			initStateMat[0][i] = sc.nextDouble();
+		}
+		
+		double[][] firstTransition = Matrix.multiply(initStateMat, transitionMat);
+		firstTransition = Matrix.multiply(firstTransition, emissionMat);
+
+		System.out.print(firstTransition.length + " " + firstTransition[0].length + " ");
+
+		for (int i = 0; i < firstTransition[0].length; i++) 
+		{
+			System.out.printf("%f ", firstTransition[0][i]);
+		}
+
+		System.out.println();
 	}
 }
